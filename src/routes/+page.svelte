29fdb7b220a649fa8af17853491e18a40e2c6dfb
ilovework111 <svyatoslavleshchenko@gmail.com>
2023-02-27 20:31:47 +0300

    deleted backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,49 @@
 <script lang="ts">
   import Cookies from 'js-cookie';
   var val = 0;
-  interface Cart {
-    [productId: number]: number;
-  }
-
-  function getCart(): Cart {
-    const cartCookie = Cookies.get('cart');
-    if (cartCookie) {
-      const cart: Cart = {};
-      const cartArray = cartCookie.split(',');
-      for (let item of cartArray) {
-        const [productIdString, quantityString] = item.split(':');
-        const productId = parseInt(productIdString);
-        const quantity = parseInt(quantityString);
-        if (!isNaN(productId) && !isNaN(quantity)) {
-          cart[productId] = quantity;
-        }
-      }
-      return cart;
-    }
-    return {};
-  }
-
-  function addToCart(productId: number) {
-    const cart: Cart = getCart();
-    cart[productId] = (cart[productId] || 0) + 1;
-    const cartArray = Object.entries(cart).map(([productId, quantity]) => `${productId}:${quantity}`);
-    Cookies.set('cart', cartArray.join(','), { expires: 7 });
-  }
-
-  function removeFromCart(productId: number) {
-    const cart: Cart = getCart();
-    if (cart[productId]) {
-      cart[productId] -= 1;
-      if (cart[productId] === 0) {
-        delete cart[productId];
-      }
-      const cartArray = Object.entries(cart).map(([productId, quantity]) => `${productId}:${quantity}`);
-      Cookies.set('cart', cartArray.join(','), { expires: 7 });
-    }
-  }
-
-  addToCart(123); 
+  var val2 = 0;
   function print(){
-    console.log(getCart());
+    const cart = Cookies.get('cart');
+    if (cart != undefined){
+      console.log(JSON.parse(cart));
+    }
   }
-
+  function addItem(id: number, quantity: number){
+    const cart = JSON.parse(Cookies.get('cart') || '{}');
+    const newEntry = {
+      ["item"+id.toString()]: quantity
+    };
+    const newCart = {
+      ...cart, 
+      ...newEntry
+    };
+    Cookies.set('cart', JSON.stringify(newCart), {expires: 7})
+  }
+  function clear(){
+    Cookies.set('cart', '{}')
+  }
 </script>
 <head>
-  <title>Sign Up</title>
+  <title>Test prod</title>
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
 </head>
-<h1>test</h1>
-<input type="number" bind:value={val}/>
-<button on:click={() => addToCart(val)}>write</button>
-<button on:click={print}>print</button>
+<body>
+  <h1>test</h1>
+  <input type="number" bind:value={val}/>
+  <input type="number" bind:value={val2}/>
+  <button on:click={() => addItem(val2, val)}>в корзину</button>
+  <button on:click={print}>print</button>
+  <button on:click={clear}>очистить корзину</button>
+</body>
 <style>
   h1 {
-    font-weight: 700;
-    font-size: 32;
+    font-weight: 900;
+    font-size: 48;
+    padding-left: 20px;
+  }
+  input {
+    width: 30px;
   }
 </style>
